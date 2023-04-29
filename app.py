a7864a717b2e123181df6c77e4586143880c1f89
Nealon Hager <nealonhager@gmail.com>
2023-04-29 13:02:16 -0700

    v1

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
+import os
+import openai
+import json
+import pyttsx3
+
+from message import Message, Prompt, Response
+from dotenv import load_dotenv
+
+load_dotenv()
+engine = pyttsx3.init()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
 def main():
-    pass
+    resp = Response(
+        openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[Prompt("what is the color of the sky?").to_dict()],
+        )["choices"][0]["message"]["content"]
+    )
+
+    print(resp.to_json())
+    engine.setProperty("rate", 150)
+    engine.save_to_file(resp.content, "./test.mp3")
+    engine.say(resp.content)
+    engine.runAndWait()
+    engine.stop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-    
---
+++ message.py
@@ -0,0 +1,24 @@
+import json
+
+
+class Message:
+    def __init__(self, role, content, name):
+        self.role = role
+        self.content = content
+        self.name = name
+
+    def to_dict(self):
+        return {"role": self.role, "content": self.content, "name": self.name}
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+
+class Prompt(Message):
+    def __init__(self, content):
+        super().__init__(role="user", content=content, name="Human")
+
+
+class Response(Message):
+    def __init__(self, content):
+        super().__init__(role="assistant", content=content, name="Amelia Isaacson")
